a28720ac3836321f9444ce31f9be2be91387aff5
takochuu <takochuu@gmail.com>
2018-11-17 20:23:36 +0900

    Fix DeadLock

--- concurrencygo/conflict/conflict.go
+++ concurrencygo/conflict/conflict.go
@@ -2,6 +2,7 @@ package conflict
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -14,4 +15,33 @@ func Conflict() {
 	if data == 0 {
 		fmt.Printf("the value is %v", data)
 	}
+}
+
+func Conflict1() {
+	var data int
+	go func() {
+		data++
+	}()
+	if data == 0 {
+		fmt.Println("the value is 0")
+	} else {
+		fmt.Printf("the value is %v", data)
+	}
+}
+
+func Conflict2() {
+	var m sync.Mutex
+	var value int
+	go func() {
+		m.Lock()
+		value++
+		m.Unlock()
+	}()
+	m.Lock()
+	if value == 0 {
+		fmt.Println("the value is 0")
+	} else {
+		fmt.Printf("the value is %v", value)
+	}
+	m.Unlock()
 }
--- concurrencygo/main.go
+++ concurrencygo/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/takochuu/sandbox/concurrencygo/conflict"
+import (
+	"github.com/takochuu/sandbox/concurrencygo/deadlock"
+)
 
 func main() {
-	conflict.Conflict()
+	//conflict.Conflict()
+	//conflict.Conflict1()
+	//conflict.Conflict2()
+	deadlock.DeadLock()
 }
